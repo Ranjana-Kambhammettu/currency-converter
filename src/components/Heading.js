f1df483980a88ef3fa1a1610b2e51be62962fd8e
Ranjana Kambhammettu <ranjanak207@gmail.com>
2021-10-01 11:33:47 +0400

    Made changes to UI and added hyperlinks

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -11,16 +11,20 @@ class Heading extends React.Component {
                     <h1>Currency Converter</h1>
                 </header>
                 <div class="byline">
-                    <h2>This is a web app that helps you convert currencies</h2>
+                    <h2>This is a web app that helps you convert currencies.</h2>
                 </div>
                 <footer>
                     <div class="left">
-                        Hacktoberfest 2021
+                         Hacktoberfest 2021
+                        <a href="https://hacktoberfest.digitalocean.com" target="_blank" rel="noopener noreferrer">
                         <img class="pic"src={Haktober} alt="Haktoberfest Logo" />
+                        </a>
                     </div>
 
                     <div class="right">
+                        <a href="https://github.com/Ranjana-Kambhammettu/currency-converter" target="_blank" rel="noopener noreferrer">
                         <img src={GitHub} alt="GitHub Logo" />
+                        </a>
                     </div>
                 </footer>
             </>
