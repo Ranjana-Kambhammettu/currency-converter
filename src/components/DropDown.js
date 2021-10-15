b717ad1a38597d989a905f09ff292c69cce779bc
Ritika Mahapatra <ritikamahapatra.201it50@nitk.edu.in>
2021-10-15 13:57:30 +0400

    update

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -3,17 +3,18 @@ import styles from "./DropDown.module.css"
 
 class DropDown extends React.Component {
     render() {
-        return(
-            <select
-                value={this.props.value}
-                className={styles.select}
-                onChange={(e) => {
+        return ( <
+            select value = { this.props.value }
+            className = { styles.select }
+            onChange = {
+                (e) => {
                     this.props.handleChange(e)
-                }}
-            >
-                <option value="INR">Indian Rupees</option>
-                <option value="dollar">USD</option>
-            </select>
+                }
+            } >
+            <
+            option value = "INR" > Indian Rupees < /option> <
+            option value = "dollar" > USD < /option> <
+            /select>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Background from "./components/Background.js"
-//import "./Reset.css"
+
+import "./Reset.css"
 import "./App.css"
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Background />
-    </React.StrictMode>,
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    Background / >
+    <
+    /React.StrictMode>,
     document.getElementById("root")
 )
