6694b040b4162a92117537c9fe313d51eef725c8
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2021-10-19 13:22:17 +0530

    Backend Development (Incomplete)

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,20 +3,50 @@ import DropDown from "./DropDown.js"
 import ResultLabel from "./ResultLabel.js"
 import styles from "./Main.module.css"
 
-class Main extends React.Component {
-    state = {
-        value: "Choose currency denomination",
-    };
+function convert(props) {
+    if (props.curr1 === '' || props.curr2 === '')
+        return '';
+    return "Convert";
+}
 
-    handleChange = (event) => {
-        this.setState({
-            value: event.target.value
-        });
+class Main extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleLabelChange = this.handleLabelChange.bind(this);
+
+        this.state = {
+            curr1: '',
+            curr2: '',
+            result: '',
+        }
+        this.currencies = {
+            USD: 'USD',
+            INR: 'INR'
+        }
+        this.conversion_rate = {
+            USD: '1',
+            INR: '70',
+        }
+    }
+
+    handleChange() {
+        console.log("Hello")
+        // this.setState({result: "Hello"});
+    }
+
+    handleLabelChange(event, index) {
+        const tmp = event.target.value;
+        if (index === '1')
+            this.setState({curr1: tmp});
+        else
+            this.setState({curr2: tmp});
     }
 
     render() {
         return(
-            <form className={styles.form}>
+            <div className={styles.form}>
                 <div className={styles.input}>
                     <label className={styles.label}>Enter your amount here:</label>
                     <input className={styles.input_amt}type="text" />
@@ -25,16 +55,23 @@ class Main extends React.Component {
                 <div className={styles.dropdown}>
                     <DropDown
                         state={this.state}
-                        handleChange={this.handleChange}/>
+                        handleChange={this.handleLabelChange}
+                        index='1'
+                        rates={this.currencies} />
                     <div className={styles.text}>to</div>
                     <DropDown
                         state={this.state}
-                        handleChange={this.handleChange}/>
+                        handleChange={this.handleLabelChange}
+                        index='2'
+                        rates={this.currencies} />
                 </div>
                 <div className={styles.result}>
-                    <ResultLabel />
+                    <ResultLabel
+                        props={this.state}
+                        handleChange={() => this.handleChange()}
+                    />
                 </div>
-            </form>
+            </div>
         );
     }
 }
